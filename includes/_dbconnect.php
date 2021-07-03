46c4e6ada34b21e488d27ed17adf55ccf96fba7e
vykhy <80080580+vykhy@users.noreply.github.com>
2021-07-03 12:42:44 +0530

    Update _dbconnect.php

--- includes/_dbconnect.php
+++ includes/_dbconnect.php
@@ -3,9 +3,9 @@
 function pdo_connect_mysql(){
     //update details with database details
     $dbhost = 'localhost';
-    $dbuser = 'root'; //vykhyotm_root
-    $dbpassword = ''; //mysitedbrootuser
-    $dbname = 'vykhyotm_forum';
+    $dbuser = 'root'; 
+    $dbpassword = ''; 
+    $dbname = '';
 
     try{
         return new PDO('mysql:host='.$dbhost.';dbname=' . $dbname . ';charset=utf8', $dbuser, $dbpassword);
