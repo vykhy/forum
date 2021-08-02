0dfa3d9d4371981c8054ab71e73981a137f2e5bd
vikehieto <vikehietoyhoshii@gmail.com>
2021-08-02 07:48:07 +0530

    updated mesage in automatic login

--- includes/_automatical_login.php
+++ includes/_automatical_login.php
@@ -2,12 +2,19 @@
 ob_start();
 
 session_start();
+
+if(isset($_SESSION['username'])){
+    $msg = 'You are already logged in';
+    header("location: ../index.php?login=$msg");
+}
+
 $_SESSION['username'] = 'Guest user';
 $_SESSION['id'] = 1;
 $_SESSION['email'] = 'guest@forum.com';
 $_SESSION['loggedin'] = true;
 
-header('location: ../index.php?login=true');
+$msg = 'Logged in successully';
+header("location: ../index.php?login=$msg");
 
 ob_end_flush();
 ?>
--- js/scripts.js
+++ js/scripts.js
@@ -52,11 +52,3 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
-
-
-document.querySelector('#logindelbtn').addEventListener('click', ()=>{
-    document.querySelector('#login').remove();
-})
-document.querySelector('#messagedelbtn').addEventListener('click', ()=>{
-    document.querySelector('#message').remove();
-})
